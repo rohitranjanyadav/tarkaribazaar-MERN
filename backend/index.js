f5011853bfae8791189eb97de2b9a50de1aacce7
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2025-10-15 21:25:36 +0545

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ import path from "path";
 connectDB();
 // middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://tarkaribazaar-frontend.onrender.com", credentials: true }));
 app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello World!");
